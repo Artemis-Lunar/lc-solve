511fb3fe74b580078810bfa28c9e417ff421b4db
Artemis_Lunar <zhaoyang2003@outlook.com>
2026-02-15 23:21:23 -0800

    Update solve for question 21 and 83

---
+++ src/MergeTwoSortedList.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MergeTwoSortedList {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode merge = new ListNode(0);
+        ListNode current = merge;
+
+        // travel both lists and merge them
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+        return merge.next;
+    }
+}
