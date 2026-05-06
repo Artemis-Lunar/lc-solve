927f7c49649cb26953cfcadc511213a11cd9e35c
Artemis-Lunar <zhaoyang2003@outlook.com>
2026-05-05 23:15:35 -0700

    Add Kth Smallest Element in a BST problem

---
+++ src/KthSmallestElementinaBST.java
@@ -0,0 +1,27 @@
+public class KthSmallestElementinaBST {
+    class Solution {
+        private int count = 0;
+        private int answer = 0;
+
+        public int kthSmallest(TreeNode root, int k) {
+            inorder(root, k);
+            return answer;
+        }
+
+        private void inorder(TreeNode node, int k) {
+            if (node == null) {
+                return;
+            }
+
+            inorder(node.left, k);
+
+            count++;
+            if (count == k) {
+                answer = node.val;
+                return;
+            }
+
+            inorder(node.right, k);
+        }
+    }
+}
